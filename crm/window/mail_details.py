e770dcb9dcebcfab269041b1c8636ea194d64f5b
rom4in_n <romain.navarro38@gmail.com>
2022-09-12 01:21:11 +0200

    New button to add a tag

--- crm/window/mail_details.py
+++ crm/window/mail_details.py
@@ -1,13 +1,16 @@
 """Module contenant la classe DetailsMail permettant de générer la fenêtre
 d'ajout ou modification d'un mail."""
+from functools import partial
 
-from PySide6.QtCore import Signal
+from PySide6.QtCore import Signal, Qt
+from PySide6.QtGui import QPixmap, QIcon
 from PySide6.QtSql import QSqlDatabase, QSqlQueryModel
-from PySide6.QtWidgets import QWidget, QFormLayout, QLineEdit, QLabel, QDataWidgetMapper, QPushButton, \
+from PySide6.QtWidgets import QWidget, QGridLayout, QLineEdit, QLabel, QDataWidgetMapper, QPushButton, \
     QVBoxLayout, QHBoxLayout, QComboBox, QSpacerItem, QSizePolicy, QMessageBox
 
-from crm.api.utils import DATA_FILE, check_mail_format
-from crm.database.client import update_mail, get_tag_to_category_mail, add_mail
+from crm.api.utils import DATA_FILE, check_mail_format, RESOURCE_DIR
+from crm.database.client import update_mail, get_tag_to_category_mail, add_mail, add_tag
+from crm.window.input_tag import InputTag
 
 
 # noinspection PyAttributeOutsideInit
@@ -26,6 +29,7 @@ class DetailsMail(QWidget):
         self.connect_db()
         self.setup_model()
         self.setup_ui()
+        self.setObjectName("details")
         self.setMinimumWidth(350)
         if mode_action == "modify":
             self.setWindowTitle("Modifier une adresse mail")
@@ -56,8 +60,11 @@ class DetailsMail(QWidget):
         self.setup_connections()
 
     def create_widgets(self):
+        self.la_mail = QLabel("Mail")
         self.le_mail = QLineEdit()
+        self.la_tag = QLabel("Tag")
         self.cbx_tag = QComboBox()
+        self.btn_new_tag = QPushButton("")
         self.btn_validate = QPushButton("Valider")
         self.btn_cancel = QPushButton("Annuler")
 
@@ -68,15 +75,22 @@ class DetailsMail(QWidget):
         self.cbx_tag.addItems(self.tags)
         if self.mode_action == "modify":
             self.cbx_tag.setCurrentText(self.tags[self.idx.index(self.model.query().value(2))])
+        self.cbx_tag.setSizePolicy(QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed))
+        self.cbx_tag.setObjectName("mail")
+        self.btn_new_tag.setIcon(QIcon(QPixmap(RESOURCE_DIR / "tag--plus.png")))
+        self.btn_new_tag.setStyleSheet("QPushButton {min-width: 0px;}")
 
     def create_layouts(self):
         self.main_layout = QVBoxLayout(self)
-        self.values_layout = QFormLayout()
+        self.values_layout = QGridLayout()
         self.btn_layout = QHBoxLayout()
 
     def add_widgets_to_layouts(self):
-        self.values_layout.addRow(QLabel("Mail"), self.le_mail)
-        self.values_layout.addRow(QLabel("Tag"), self.cbx_tag)
+        self.values_layout.addWidget(QLabel("Mail"), 0, 0, 1, 1)
+        self.values_layout.addWidget(self.le_mail, 0, 1, 1, 2)
+        self.values_layout.addWidget(QLabel("Tag"), 1, 0, 1, 1)
+        self.values_layout.addWidget(self.cbx_tag, 1, 1, 1, 1)
+        self.values_layout.addWidget(self.btn_new_tag, 1, 2, 1, 1)
 
         self.btn_layout.addItem(QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum))
         self.btn_layout.addWidget(self.btn_validate)
@@ -87,8 +101,23 @@ class DetailsMail(QWidget):
         self.main_layout.addLayout(self.btn_layout)
 
     def setup_connections(self):
+        self.btn_new_tag.clicked.connect(self.open_new_tag)
         self.btn_validate.clicked.connect(self.save_changes)
         self.btn_cancel.clicked.connect(self.close)
+
+    def open_new_tag(self):
+        """Ouvre la fenêtre de saisi utilisateur pour ajouter un tag."""
+        category = self.cbx_tag.objectName()
+        self.new_tag = InputTag(self, "adding", category)
+        self.new_tag.setWindowModality(Qt.ApplicationModal)
+        self.new_tag.save_tag.connect(partial(self.add_tag, category))
+        self.new_tag.show()
+
+    def add_tag(self, category: str, new_tag: str):
+        id_ = add_tag(tag=new_tag, category=category)
+        self.tags.append(new_tag)
+        self.idx.append(id_)
+        self.cbx_tag.addItem(new_tag)
 
     def save_changes(self):
         """Sauvegarde en bdd du mail et du tag associé après vérifications"""
